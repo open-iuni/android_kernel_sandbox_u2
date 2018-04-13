f0a08abe5454a1f5f78d17fce2066b51b2082c1a
Abdessamad DERRAZ <derraz.abdessamad@gmail.com>
2018-04-13 18:50:20 +0200

    media: camera: add OV16825 and OV4688

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -995,7 +995,20 @@ int msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl, void __user *argp)
 			rc = -EFAULT;
 		}
 		break;
-
+    //Gionee liushengbin 20131108 modify for ov4688 otp start
+    case CFG_SET_INIT_SETTING:
+         if(s_ctrl->gn_otp_func_tbl != NULL) {
+                    rc = s_ctrl->gn_otp_func_tbl->gn_sensor_otp_support(s_ctrl);
+                    if (rc != 1) {
+                            pr_err("%s: %s: sensor otp support failed\n", __func__,
+                                    s_ctrl->sensordata->sensor_name);
+                            return rc;
+                    }
+					printk("%s: %s: sensor otp support success\n", __func__,
+                                    s_ctrl->sensordata->sensor_name);
+            }
+            break;
+    //Gionee liushengbin 20131108 modify for ov4688 otp end
 	case CFG_SET_STOP_STREAM_SETTING: {
 		struct msm_camera_i2c_reg_setting *stop_setting =
 			&s_ctrl->stop_setting;
